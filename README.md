5473503999ef65298680b81af3fdf3853b221dde
agungardiyanta <agungardiyanta@gmail.com>
2024-08-28 22:03:02 +0700

    update readme and deployment

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # UrlShorterDeployment
+Repository ini bagian dari repositoy [UrlShorter](https://github.com/agungardiyanta/UrlShorter) yang menyimpan infrastructure code atau script yang digunakan.
+
+UrlShorter project merupakan aplikasi yang menyediakan layanan untuk mengubah atau memperpendek link menjadi `short_id` yang unik.\
+Project ini dibagi menjadi 3 service utama frontend, core dan analytic
+* frontend service sebagai tampilan web menggunakan vite.js + react
+Lalu ada dua backend service menggunakan bahasa Golang 1.22
+* core service merupakan API backend yang membuat dan melakukan redirect link dengan short_id 
+* analytic service merupakan API backend yang menyimpan berapa banyak total sebuah link short_id diakses
+
+## Infrastructure
+Project ini di deploy dalam cluster Google Kubernetes Engine dengan konfigurasi 2 node 
+
+
--- k6s/get-script.js
+++ k6s/get-script.js
@@ -1,13 +1,16 @@
 import http from "k6/http";
+import { sleep } from 'k6';
+import { check } from 'k6';
 
 export const options = {
   // A number specifying the number of VUs to run concurrently.
   insecureSkipTLSVerify: true,
   stages: [
-    { duration: "5s", target: 100 },
-    { duration: "10s", target: 500 },
-    { duration: "5s", target: 10000 },
-    { duration: "10s", target: 10 }
+    { duration: '30s', target: 500 },   // Ramp-up to 500 RPS
+    { duration: '1m', target: 500 },   // Stay at 500 RPS for 3 minutes
+    { duration: '10s', target: 10000 }, // Spike to 10,000 RPS
+    { duration: '30s', target: 500 },   // Ramp-down back to 500 RPS
+    { duration: '10s', target: 0 },
   ],
 };
 
@@ -20,5 +23,7 @@ export default function () {
   var randomShortId = getRandomElement(short_id_list);
 
   const url = `https://dsandbox.online/api/analytic/stats/${randomShortId}`;
-  http.get(url);
+  const res = http.get(url);
+  check(res, { 'status is 200': (r) => r.status === 200 });
+  sleep(1);
 }
