99aa0a729291fdd32b8a7a07fadb8ac1d23a592b
agungardiyanta <agungardiyanta@gmail.com>
2024-08-28 21:30:42 +0700

    add k6s script and update hpa

--- k6s/get-script.js
+++ k6s/get-script.js
@@ -4,10 +4,10 @@ export const options = {
   // A number specifying the number of VUs to run concurrently.
   insecureSkipTLSVerify: true,
   stages: [
-    { duration: "10s", target: 500 }, // ramp up to 400 users
-    { duration: "10s", target: 1000 }, // stay at 400 for ~4 hours
+    { duration: "5s", target: 100 },
+    { duration: "10s", target: 500 },
     { duration: "5s", target: 10000 },
-    { duration: "5s", target: 0 }, // scale down. (optional)
+    { duration: "10s", target: 10 }
   ],
 };
 
@@ -19,6 +19,6 @@ export default function () {
   const short_id_list = ["2lHzWyUZ", "2lHzfPCO", "hdjdjd"];
   var randomShortId = getRandomElement(short_id_list);
 
-  const url = `https://dsandbox.online/s/${randomShortId}`;
+  const url = `https://dsandbox.online/api/analytic/stats/${randomShortId}`;
   http.get(url);
 }
